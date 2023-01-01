f3febdbcf182a682f357dc41ad2e5c49b10c522e
coolcatco888 <coolcatco888@hotmail.com>
2022-12-31 18:06:17 -0800

    Created Threaded Capture Object

---
+++ src/prototype/display.py
@@ -0,0 +1,75 @@
+# Real-Time Camera
+# https://stackoverflow.com/questions/55828451/video-streaming-from-ip-camera-in-python-using-opencv-cv2-videocapture
+# https://stackoverflow.com/questions/58293187/opencv-real-time-streaming-video-capture-is-slow-how-to-drop-frames-or-get-sync
+from threading import Thread
+import time, cv2
+
+class VideoStreamCapture(object):
+    def __init__(self, src=0):
+        # Create a VideoCapture object
+        self.capture = cv2.VideoCapture(src)
+        self.capture.set(cv2.CAP_PROP_BUFFERSIZE, 2)
+       
+        # FPS = 1/X
+        # X = desired FPS
+        self.FPS = 1/60
+        self.FPS_MS = int(self.FPS * 1000)
+
+        # Start the thread to read frames from the video stream
+        self.thread = Thread(target=self.update, args=())
+        self.thread.daemon = True
+        self.thread.start()
+
+    def update(self):
+        # Read the next frame from the stream in a different thread
+        while True:
+            if self.capture.isOpened():
+                (self.status, self.frame) = self.capture.read()
+            time.sleep(self.FPS)
+
+    def show_frame(self):
+        # Display frames in main program
+        if self.status:
+            self.frame = self.maintain_aspect_ratio_resize(self.frame, width=1080)
+            cv2.imshow('IP Camera Video Streaming', self.frame)
+
+        # Press Q on keyboard to stop recording
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            self.capture.release()
+            cv2.destroyAllWindows()
+            exit(1)
+
+    # Resizes a image and maintains aspect ratio
+    def maintain_aspect_ratio_resize(self, image, width=None, height=None, inter=cv2.INTER_AREA):
+        # Grab the image size and initialize dimensions
+        dim = None
+        (h, w) = image.shape[:2]
+
+        # Return original image if no need to resize
+        if width is None and height is None:
+            return image
+
+        # We are resizing height if width is none
+        if width is None:
+            # Calculate the ratio of the height and construct the dimensions
+            r = height / float(h)
+            dim = (int(w * r), height)
+        # We are resizing width if height is none
+        else:
+            # Calculate the ratio of the 0idth and construct the dimensions
+            r = width / float(w)
+            dim = (width, int(h * r))
+
+        # Return the resized image
+        return cv2.resize(image, dim, interpolation=inter)
+
+if __name__ == '__main__':
+    stream_link = 0
+    video_stream_widget = VideoStreamCapture(stream_link)
+    while True:
+        try:
+            video_stream_widget.show_frame()
+        except AttributeError:
+            pass
+    
