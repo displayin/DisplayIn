5e4c646d77c6760b7b9ced0b2fcf6d23a52ae0bd
coolcatco888 <coolcatco888@hotmail.com>
2023-01-20 21:39:01 -0800

    Added windows devenv setup script

---
+++ scripts/setup_devenv_windows.ps1
@@ -0,0 +1,11 @@
+# Install OpenCV
+pip install opencv-python
+
+# Install SoundDevice
+pip install sounddevice
+
+# Install PyInstaller
+pip install pyinstaller
+
+# Install PyGObject
+pip install PyGObject
