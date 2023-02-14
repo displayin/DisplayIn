5ca2672824675dc6fb69a95cb7551c67c4a195d3
coolcatco888 <coolcatco888@hotmail.com>
2023-02-13 23:00:29 -0800

    Added FFMPEG to prereq

--- scripts/setup_devenv_windows_msys2.sh
+++ scripts/setup_devenv_windows_msys2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Note this must be run within msys2 mingw64
-pacman -Suy mingw-w64-x86_64-gtk3 mingw-w64-x86_64-python3 mingw-w64-x86_64-python3-gobject mingw-w64-x86_64-python-pip mingw-w64-x86_64-python-sounddevice mingw-w64-x86_64-portaudio mingw-w64-x86_64-opencv mingw-w64-x86_64-python-opencv mingw-w64-x86_64-gstreamer
+pacman -Suy mingw-w64-x86_64-gtk3 mingw-w64-x86_64-python3 mingw-w64-x86_64-python3-gobject mingw-w64-x86_64-python-pip mingw-w64-x86_64-python-sounddevice mingw-w64-x86_64-portaudio mingw-w64-x86_64-opencv mingw-w64-x86_64-python-opencv mingw-w64-x86_64-gstreamer mingw-w64-x86_64-ffmpeg mingw-w64-x86_64-python-ffmpeg-python
 
 # Install Debugpy
 pip install debugpy
