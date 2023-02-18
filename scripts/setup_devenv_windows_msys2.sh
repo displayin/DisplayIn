df09ea81b87930771b7d3673b59c4b5f9cfe72fa
coolcatco888 <coolcatco888@hotmail.com>
2023-02-17 22:36:06 -0800

    Updated prereq script to remove opencv

--- scripts/setup_devenv_windows_msys2.sh
+++ scripts/setup_devenv_windows_msys2.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # Note this must be run within msys2 mingw64
-pacman -Suy mingw-w64-x86_64-gtk3 mingw-w64-x86_64-python3 mingw-w64-x86_64-python3-gobject mingw-w64-x86_64-python-pip mingw-w64-x86_64-python-sounddevice mingw-w64-x86_64-portaudio git mingw-w64-x86_64-python-opencv
+pacman -Suy mingw-w64-x86_64-gtk3 mingw-w64-x86_64-python3 mingw-w64-x86_64-python3-gobject mingw-w64-x86_64-python-pip mingw-w64-x86_64-python-sounddevice mingw-w64-x86_64-portaudio mingw-w64-x86_64-cmake mingw-w64-x86_64-gcc mingw-w64-x86_64-toolchain git
 
 # Unused Packages
-# mingw-w64-x86_64-opencv pac mingw-w64-x86_64-gstreamer mingw-w64-x86_64-ffmpeg mingw-w64-x86_64-python-ffmpeg-python mingw-w64-x86_64-cmake mingw-w64-x86_64-gcc mingw-w64-x86_64-toolchai
+# mingw-w64-x86_64-opencv pac mingw-w64-x86_64-gstreamer mingw-w64-x86_64-ffmpeg mingw-w64-x86_64-python-ffmpeg-python mingw-w64-x86_64-python-opencv
+
+# Remove opencv
+# pacman -R mingw-w64-x86_64-x264 mingw-w64-x86_64-ffms2 mingw-w64-x86_64-ffmpeg mingw-w64-x86_64-frei0r-plugins mingw-w64-x86_64-opencv
 
 # Install Debugpy
 pip install debugpy
