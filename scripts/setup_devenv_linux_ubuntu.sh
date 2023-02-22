9157d63869e2788232049154a104209bcbd93411
coolcatco888 <coolcatco888@hotmail.com>
2023-02-21 18:42:05 -0800

    Added ubuntu dev environment setup script

---
+++ scripts/setup_devenv_linux_ubuntu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt install python3-gi python3-gi-cairo gir1.2-gtk-3.0 python-is-python3 python3-pip python3-opencv
+
+# Install Debugpy
+sudo pip install debugpy
+
+# Install PyInstaller
+sudo pip install pyinstaller
+
+# Install sounddevice
+sudo pip install sounddevice
