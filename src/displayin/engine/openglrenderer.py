cdec273eeee1122120022519e0aa31e4ad21db91
coolcatco888 <coolcatco888@hotmail.com>
2023-02-28 12:51:06 -0800

    Changed filter to linear

--- src/displayin/engine/openglrenderer.py
+++ src/displayin/engine/openglrenderer.py
@@ -149,8 +149,8 @@ class OpenGLRenderer(Gtk.GLArea):
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
 
             # Set texture Filtering parameter
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
 
             glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, w, h, 0, GL_RGB, GL_UNSIGNED_BYTE, pixels)
             glGenerateMipmap(GL_TEXTURE_2D)
