6d3eed1ea7c1e6059d8e70c7e064ee538ffde718
coolcatco888 <coolcatco888@hotmail.com>
2023-03-02 15:39:44 -0800

    Added cv2 patch in windows build Still need to fix

--- src/displayin/build_win.sh
+++ src/displayin/build_win.sh
@@ -10,5 +10,7 @@
 # Run Pyinstaller
 pyinstaller --noconsole --add-data="resource/ui/maingui.glade;resource/ui" --onedir displayin.py
 
-# Workaround to manually copy cv2 standalone python to dynamic-load library modules
-cp ../../../opencv-4.7.0/build/lib/python3/cv2*.pyd dist/displayin/lib-dynload/
+# Workaround to manually copy cv2 standalone python and module config files to library modules output
+mkdir dist/displayin/cv2/python3.10/
+cp install/patch/*py dist/displayin/cv2/
+cp ../../../opencv-4.7.0/build/lib/python3/cv2*.pyd dist/displayin/cv2/python3.10/
