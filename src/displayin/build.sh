18d1fe92146962f0cd8dc10accde039f21c73492
coolcatco888 <coolcatco888@hotmail.com>
2023-03-02 13:57:53 -0800

    Added cleanup before build.

--- src/displayin/build.sh
+++ src/displayin/build.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+
+# Clean Build Folders
+[ -d "build" ] && rm -rf build/*
+[ -d "dist" ] && rm -rf build/*
+
+# Run Pyinstaller
 pyinstaller --noconsole --add-data="resource/ui/maingui.glade;resource/ui" displayin.py
--- src/displayin/build_win.sh
+++ src/displayin/build_win.sh
@@ -3,6 +3,10 @@
 # Experimental Paths
 #  --paths "../../../opencv-4.7.0/build/install/x64/mingw/bin/" --paths "../../../opencv-4.7.0/build/install/x64/mingw/lib/" --paths "/mingw64/lib/python3.10/site-packages/cv2/python-3.10/"
 
+# Clean Build Folders
+[ -d "build" ] && rm -rf build/*
+[ -d "dist" ] && rm -rf build/*
+
 # Run Pyinstaller
 pyinstaller --noconsole --add-data="resource/ui/maingui.glade;resource/ui" --onedir displayin.py
 
