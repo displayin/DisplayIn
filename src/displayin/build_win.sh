302d87d7837c69f47ebf5495f9822d4e51f9424a
coolcatco888 <coolcatco888@hotmail.com>
2023-03-02 13:10:30 -0800

    Fix dynamic-load cv2 in windows build script

--- src/displayin/build_win.sh
+++ src/displayin/build_win.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-#  --paths "../../../opencv-4.7.0/build/install/x64/mingw/bin/" --paths "../../../opencv-4.7.0/build/install/x64/mingw/lib/"
-pyinstaller --noconsole --add-data="resource/ui/maingui.glade;resource/ui" --onedir displayin.py --paths "/mingw64/lib/python3.10/site-packages/cv2/python-3.10/"
+
+# Experimental Paths
+#  --paths "../../../opencv-4.7.0/build/install/x64/mingw/bin/" --paths "../../../opencv-4.7.0/build/install/x64/mingw/lib/" --paths "/mingw64/lib/python3.10/site-packages/cv2/python-3.10/"
+
+# Run Pyinstaller
+pyinstaller --noconsole --add-data="resource/ui/maingui.glade;resource/ui" --onedir displayin.py
+
+# Workaround to manually copy cv2 standalone python to dynamic-load library modules
+cp ../../../opencv-4.7.0/build/lib/python3/cv2*.pyd dist/displayin/lib-dynload/
