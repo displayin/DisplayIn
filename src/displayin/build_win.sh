11674b3c0e051b93b939a095ba9c5985c300b2bc
coolcatco888 <coolcatco888@hotmail.com>
2023-03-01 18:40:34 -0800

    Still work in progress win build script

--- src/displayin/build_win.sh
+++ src/displayin/build_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-pyinstaller --noconsole --add-data="resource/ui/maingui.glade;resource/ui" --onedir displayin.py --paths "../../../opencv-4.7.0/build/install/x64/mingw/bin/" --paths "../../../opencv-4.7.0/build/install/x64/mingw/bin/" --paths "/mingw64/lib/python3.10/site-packages/cv2/python-3.10/"
+#  --paths "../../../opencv-4.7.0/build/install/x64/mingw/bin/" --paths "../../../opencv-4.7.0/build/install/x64/mingw/lib/"
+pyinstaller --noconsole --add-data="resource/ui/maingui.glade;resource/ui" --onedir displayin.py --paths "/mingw64/lib/python3.10/site-packages/cv2/python-3.10/"
