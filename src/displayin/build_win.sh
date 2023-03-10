984eeffbb20f3ab1e786c7ca542302d8ac598a80
coolcatco888 <coolcatco888@hotmail.com>
2023-03-09 16:49:05 -0800

    Fixed Windows build script

--- src/displayin/build_win.sh
+++ src/displayin/build_win.sh
@@ -11,6 +11,6 @@
 pyinstaller --noconsole --add-data="resource/ui/maingui.glade;resource/ui" --onedir displayin.py
 
 # Workaround to manually copy cv2 standalone python and module config files to library modules output
-mkdir dist/displayin/cv2/python3.10/
+mkdir dist/displayin/cv2/python-3.10/
 cp install/patch/*py dist/displayin/cv2/
-cp ../../../opencv-4.7.0/build/lib/python3/cv2*.pyd dist/displayin/cv2/python3.10/
+cp ../../../opencv-4.7.0/build/lib/python3/cv2*.pyd dist/displayin/cv2/python-3.10/
