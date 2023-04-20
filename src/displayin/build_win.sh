8041c67dfc4d11543adcbeed1ec48241e43a8ec4
coolcatco888 <coolcatco888@hotmail.com>
2023-04-20 15:37:00 -0700

    Made build script more portable

--- src/displayin/build_win.sh
+++ src/displayin/build_win.sh
@@ -7,6 +7,10 @@
 [ -d "build" ] && rm -rf build/*
 [ -d "dist" ] && rm -rf dist/*
 
+# Set GSTREAMER ROOT
+GSTREAMER_ROOT=/c/gstreamer
+[ -d "/f/gstreamer" ] && GSTREAMER_ROOT=/f/gstreamer
+
 # Run Pyinstaller
 pyinstaller --noconsole --add-data="resource/ui/maingui.glade;resource/ui" --onedir displayin.py
 
@@ -30,18 +34,18 @@ cp /mingw64/bin/libImath-3_1.dll dist/displayin/lib/opencv/bin/
 cp /mingw64/bin/libOpenEXR-3_1.dll dist/displayin/lib/opencv/bin/
 
 # Copy gstreamer dependencies
-cp /f/gstreamer/1.0/msvc_x86_64/bin/gstapp-1.0-0.dll dist/displayin/lib/opencv/bin/
-cp /f/gstreamer/1.0/msvc_x86_64/bin/gstaudio-1.0-0.dll dist/displayin/lib/opencv/bin/
-cp /f/gstreamer/1.0/msvc_x86_64/bin/gstbase-1.0-0.dll dist/displayin/lib/opencv/bin/
-cp /f/gstreamer/1.0/msvc_x86_64/bin/gstpbutils-1.0-0.dll dist/displayin/lib/opencv/bin/
-cp /f/gstreamer/1.0/msvc_x86_64/bin/gstreamer-1.0-0.dll dist/displayin/lib/opencv/bin/
-cp /f/gstreamer/1.0/msvc_x86_64/bin/gstriff-1.0-0.dll dist/displayin/lib/opencv/bin/
-cp /f/gstreamer/1.0/msvc_x86_64/bin/gsttag-1.0-0.dll dist/displayin/lib/opencv/bin/
-cp /f/gstreamer/1.0/msvc_x86_64/bin/gstvideo-1.0-0.dll dist/displayin/lib/opencv/bin/
-cp /f/gstreamer/1.0/msvc_x86_64/bin/gstd3d11-1.0-0.dll dist/displayin/lib/opencv/bin/
-cp /f/gstreamer/1.0/msvc_x86_64/bin/orc-0.4-0.dll dist/displayin/lib/opencv/bin/
+cp $GSTREAMER_ROOT/1.0/msvc_x86_64/bin/gstapp-1.0-0.dll dist/displayin/lib/opencv/bin/
+cp $GSTREAMER_ROOT/1.0/msvc_x86_64/bin/gstaudio-1.0-0.dll dist/displayin/lib/opencv/bin/
+cp $GSTREAMER_ROOT/1.0/msvc_x86_64/bin/gstbase-1.0-0.dll dist/displayin/lib/opencv/bin/
+cp $GSTREAMER_ROOT/1.0/msvc_x86_64/bin/gstpbutils-1.0-0.dll dist/displayin/lib/opencv/bin/
+cp $GSTREAMER_ROOT/1.0/msvc_x86_64/bin/gstreamer-1.0-0.dll dist/displayin/lib/opencv/bin/
+cp $GSTREAMER_ROOT/1.0/msvc_x86_64/bin/gstriff-1.0-0.dll dist/displayin/lib/opencv/bin/
+cp $GSTREAMER_ROOT/1.0/msvc_x86_64/bin/gsttag-1.0-0.dll dist/displayin/lib/opencv/bin/
+cp $GSTREAMER_ROOT/1.0/msvc_x86_64/bin/gstvideo-1.0-0.dll dist/displayin/lib/opencv/bin/
+cp $GSTREAMER_ROOT/1.0/msvc_x86_64/bin/gstd3d11-1.0-0.dll dist/displayin/lib/opencv/bin/
+cp $GSTREAMER_ROOT/1.0/msvc_x86_64/bin/orc-0.4-0.dll dist/displayin/lib/opencv/bin/
 
 # Copy gstreamer lib dependencies
-cp /f/gstreamer/1.0/msvc_x86_64/lib/gstreamer-1.0/gstapp.dll dist/displayin/lib/opencv/lib/gstreamer-1.0/
-cp /f/gstreamer/1.0/msvc_x86_64/lib/gstreamer-1.0/gstmediafoundation.dll dist/displayin/lib/opencv/lib/gstreamer-1.0/
-cp /f/gstreamer/1.0/msvc_x86_64/lib/gstreamer-1.0/gstvideoconvertscale.dll dist/displayin/lib/opencv/lib/gstreamer-1.0/
+cp $GSTREAMER_ROOT/1.0/msvc_x86_64/lib/gstreamer-1.0/gstapp.dll dist/displayin/lib/opencv/lib/gstreamer-1.0/
+cp $GSTREAMER_ROOT/1.0/msvc_x86_64/lib/gstreamer-1.0/gstmediafoundation.dll dist/displayin/lib/opencv/lib/gstreamer-1.0/
+cp $GSTREAMER_ROOT/1.0/msvc_x86_64/lib/gstreamer-1.0/gstvideoconvertscale.dll dist/displayin/lib/opencv/lib/gstreamer-1.0/
