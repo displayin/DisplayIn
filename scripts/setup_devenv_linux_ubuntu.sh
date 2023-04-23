ee68394b504631d9b2be897959e9fb4e33ff5bd7
coolcatco888 <coolcatco888@hotmail.com>
2023-04-23 11:38:02 -0700

    Updated linux dev setup script to include tkinter

--- scripts/setup_devenv_linux_ubuntu.sh
+++ scripts/setup_devenv_linux_ubuntu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt install python3-gi python3-gi-cairo gir1.2-gtk-3.0 python-is-python3 python3-pip python3-opencv
+sudo apt install python3-gi python3-gi-cairo gir1.2-gtk-3.0 python-is-python3 python3-pip python3-opencv python3-tk
 
 # Install Debugpy
 sudo pip install debugpy
