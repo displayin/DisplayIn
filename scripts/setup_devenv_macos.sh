97e5c090ef027c2690966f2a4b27dd3c9c58578f
coolcatco888 <coolcatco888@hotmail.com>
2023-04-24 18:21:17 -0700

    Added mac devenv setup script

---
+++ scripts/setup_devenv_macos.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Install Git
+brew install git
+
+# Install VS Code
+brew install --cask visual-studio-code
+
+# Install Python
+brew install python@3.11
+
+# Install GTK3
+brew install gtk+3
+
+# Install PyGObject
+brew install pygobject3
+
+# Install OpenCV
+brew install opencv
+
+# Install PyInstaller
+brew install pyinstaller
+
+# Set Default Python
+ln -s -f /usr/local/bin/python3.11 /usr/local/bin/python
+
+# Upgrade PIP
+python3 -m pip install --upgrade pip
+
+# Install OpenCV Python
+python3 -m pip install opencv-python
+
+# Install Debugpy
+python3 -m pip install debugpy
+
+# Install sounddevice
+python3 -m pip install sounddevice
