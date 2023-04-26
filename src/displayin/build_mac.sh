168fd33351d86af91394810a9cb91848c96657ca
coolcatco888 <coolcatco888@hotmail.com>
2023-04-25 17:49:19 -0700

    Added Info.plist patch to fix crashing

--- src/displayin/build_mac.sh
+++ src/displayin/build_mac.sh
@@ -6,3 +6,9 @@
 
 # Run Pyinstaller
 pyinstaller --noconsole --add-data="resource/ui/maingui.glade:resource/ui" --icon="resource/images/DisplayInIcon.ico" --onedir displayin.py
+
+# Patch Info.plist - Needed for security to tell the OS that we need camera access otherwise it will crash
+plistPatch="	<key>NSCameraUsageDescription</key>\n	<string>DisplayIn uses Cameras</string>\n</dict>"
+dictString="</dict>"
+sed -i '' "s|$dictString|$plistPatch|gi" "dist/displayin.app/Contents/Info.plist"
+echo "Patched NSCameraUsageDescription into Info.plist"
