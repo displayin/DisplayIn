3348b1eef2f61519533b514638b02d78e36afa90
coolcatco888 <coolcatco888@hotmail.com>
2023-08-07 21:42:41 -0700

    Work in progress export progress bar

--- src/displayin/util/videoexporter.py
+++ src/displayin/util/videoexporter.py
@@ -1,10 +1,17 @@
 from util.resource import Resource as res
 from threading import Thread
 import ffmpeg
+import time
+import os
+
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk, GLib
 
 class VideoExporter:
     def __init__(self, window):
         self.window = window
+        self.running = False
         pass
 
     def exportVideo(self, recordedFps, videoFileName="temp.avi", audioFileName="temp.wav"):
@@ -14,16 +21,28 @@ class VideoExporter:
         videoInfo = ffmpeg.probe(videoFileName)
         totalFrames = int(videoInfo['streams'][0]['nb_frames'])
 
+        # Create Progress Window
+        progressWindow = Gtk.Window(title="Exporting Video File...")
+        progressWindow.set_border_width(10)
+        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        progressWindow.add(vbox)
+        progressBar = Gtk.ProgressBar()
+        vbox.pack_start(progressBar, True, True, 0)
+        progressWindow.show_all()
+
         outFileName = res.saveFileDialog()
         if outFileName != None:
             saveThread = Thread(target=self.ffmpegExport, args=(
                 videoFileName, audioFileName, videoFile, audioFile, outFileName))
             saveThread.start()
+            progressThread = Thread(target=self.displayProgress, args=(totalFrames, progressWindow, progressBar))
+            progressThread.start()
         
         pass
 
     def ffmpegExport(self, videoFileName, audioFileName, videoFile, audioFile, outFileName):
         self.window.buttonRecord.set_sensitive(False)
+        self.running = True
         (ffmpeg
             .output(videoFile, audioFile, outFileName)
             .global_args('-progress', 'progress.txt')
@@ -31,4 +50,18 @@ class VideoExporter:
         
         res.deleteFileIfExists(videoFileName)
         res.deleteFileIfExists(audioFileName)
+        self.running = False
         self.window.buttonRecord.set_sensitive(True)
+
+    def displayProgress(self, totalFrames, progressWindow, progressBar):
+        
+        while self.running:
+            time.sleep(0.1)
+            if os.path.exists("progress.txt"):
+                progressfile = open("progress.txt", "r")
+                data = progressfile.read()
+                progressfile.close()
+
+        progressWindow.destroy()
+
+        pass
