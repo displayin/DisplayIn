6f12a08fac6b8c23dd18d2a4c3b85221fabc7715
coolcatco888 <coolcatco888@hotmail.com>
2023-08-08 12:03:45 -0700

    added ffmpeg back to windows prereq

--- scripts/setup_devenv_windows_msys2.sh
+++ scripts/setup_devenv_windows_msys2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Note this must be run within msys2 mingw64
-pacman -Suy mingw-w64-x86_64-gtk3 mingw-w64-x86_64-python3 mingw-w64-x86_64-python3-gobject mingw-w64-x86_64-python-pip mingw-w64-x86_64-python-sounddevice mingw-w64-x86_64-portaudio mingw-w64-x86_64-cmake mingw-w64-x86_64-gcc mingw-w64-x86_64-toolchain git mingw-w64-x86_64-zlib zlib mingw-w64-x86_64-python-pyopengl mingw-w64-x86_64-glade
+pacman -Suy mingw-w64-x86_64-gtk3 mingw-w64-x86_64-python3 mingw-w64-x86_64-python3-gobject mingw-w64-x86_64-python-pip mingw-w64-x86_64-python-sounddevice mingw-w64-x86_64-portaudio mingw-w64-x86_64-cmake mingw-w64-x86_64-gcc mingw-w64-x86_64-toolchain git mingw-w64-x86_64-zlib zlib mingw-w64-x86_64-python-pyopengl mingw-w64-x86_64-glade mingw-w64-x86_64-ffmpeg mingw-w64-x86_64-python-ffmpeg-python
 
 # Unused Packages
-# mingw-w64-x86_64-opencv pac mingw-w64-x86_64-gstreamer mingw-w64-x86_64-ffmpeg mingw-w64-x86_64-python-ffmpeg-python mingw-w64-x86_64-python-opencv
+# mingw-w64-x86_64-opencv pac mingw-w64-x86_64-gstreamer mingw-w64-x86_64-python-opencv
 
 # Remove opencv
 # pacman -R mingw-w64-x86_64-x264 mingw-w64-x86_64-ffms2 mingw-w64-x86_64-ffmpeg mingw-w64-x86_64-frei0r-plugins mingw-w64-x86_64-opencv
