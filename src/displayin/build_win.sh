d284302dc186af9ce65ef490e0f2f15c41f80fdd
coolcatco888 <coolcatco888@hotmail.com>
2023-08-09 11:25:42 -0700

    Added small fix to the build script

--- src/displayin/build_win.sh
+++ src/displayin/build_win.sh
@@ -15,7 +15,7 @@ GSTREAMER_ROOT=/c/gstreamer
 pyinstaller --noconsole --add-data="resource/ui/maingui.glade;resource/ui" --splash "resource/images/DisplayInSplash.png" --icon="resource/images/DisplayInIcon.ico" --onedir displayin.py
 
 # Workaround to manually copy cv2 standalone python and module config files to library modules output
-mkdir dist/displayin/cv2/python-3.11/
+[ ! -d "dist/displayin/cv2/python-3.11/" ] && mkdir dist/displayin/cv2/python-3.11/
 cp install/patch/*py dist/displayin/cv2/
 cp ../../../opencv-4.7.0/build/lib/python3/cv2*.pyd dist/displayin/cv2/python-3.11/
 
