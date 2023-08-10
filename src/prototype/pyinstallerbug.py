08d94f0ea747575cc14fed3a30d267389caa1c08
coolcatco888 <coolcatco888@hotmail.com>
2023-08-10 12:04:33 -0700

    Updated minimal repro

--- src/prototype/pyinstallerbug.py
+++ src/prototype/pyinstallerbug.py
@@ -3,7 +3,10 @@ import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, GdkPixbuf
 
-pixbuf = GdkPixbuf.Colorspace.RGB
+if False:
+    h, w, d = frame.shape
+    pixbuf = GdkPixbuf.Pixbuf.new_from_data(
+        frame.tostring(), GdkPixbuf.Colorspace.RGB, False, 8, w, h, w*d)
 
 win = Gtk.Window()
 win.show_all()
