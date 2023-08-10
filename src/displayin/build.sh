7af6845e7483f9f258bee399fe7e101a0e68504a
coolcatco888 <coolcatco888@hotmail.com>
2023-08-09 17:30:35 -0700

    Remove spec files for clean run of pyinstaller

--- src/displayin/build.sh
+++ src/displayin/build.sh
@@ -3,6 +3,7 @@
 # Clean Build Folders
 [ -d "build" ] && rm -rf build/*
 [ -d "dist" ] && rm -rf dist/*
+[ -f "displayin.spec" ] && rm displayin.spec
 
 # Run Pyinstaller
 pyinstaller --noconsole --add-data="resource/ui/maingui.glade:resource/ui" --splash "resource/images/DisplayInSplash.png" --icon="resource/images/DisplayInIcon.ico" --onedir displayin.py
--- src/displayin/build_mac.sh
+++ src/displayin/build_mac.sh
@@ -3,6 +3,7 @@
 # Clean Build Folders
 [ -d "build" ] && rm -rf build/*
 [ -d "dist" ] && rm -rf dist/*
+[ -f "displayin.spec" ] && rm displayin.spec
 
 # Run Pyinstaller
 pyinstaller --noconsole --add-data="resource/ui/maingui.glade:resource/ui" --icon="resource/images/DisplayInIcon.ico" --onedir displayin.py
